47ea577d61111ea90983c8583def7f9d7cf68667
rsmaurilho <rsmaurilho@gmail.com>
2022-06-09 19:01:59 -0300

    CriaçÃão do App Soccer News

---
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -0,0 +1,31 @@
+package me.dio.soccernews.ui.news;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soccernews.domain.News;
+
+public class NewsViewModel extends ViewModel {
+
+    private final MutableLiveData<List<News>> news;
+
+    public NewsViewModel() {
+        this.news = new MutableLiveData<>();
+
+        //TODO remover o mock de noticias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Ferroviaria tem Desfalque Importante","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+        news.add(new News("Ferroviaria Joga no Sabado","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+        news.add(new News("Copa do Mundo Feminina Esta Terminando","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."));
+
+        this.news.setValue(news);
+    }
+
+    public LiveData<List<News>> getNews() {
+        return this.news;
+    }
+}
